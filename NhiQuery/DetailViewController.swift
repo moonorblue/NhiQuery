5a5829f792e8d1336279546485b557e7030b26eb
KaeJer Cho <moonorblue@gmail.com>
2020-02-26 12:10:01 +0800

    Initial Commit

---
+++ NhiQuery/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  NhiQuery
+//
+//  Created by KaeJer Cho on 2020/2/26.
+//  Copyright © 2020 KaeJer Cho. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
